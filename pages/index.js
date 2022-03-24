15d3b259a691554d702a11be9f1426d1d8662534
Yao Song <yao.song@keeptruckin.com>
2022-03-24 12:00:32 -0700

    add column sort

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,47 @@
-import React from "react";
-import {data} from "/data/data.js";
+import React, { useState } from "react";
+import { data } from "/data/data.js";
 import Header from "../components/header";
 import utilStyles from "../styles/utils.module.css";
-import {formatTime} from "../utils";
+import { formatTime } from "../utils";
 
 const HomePage = () => {
+  const [titleDirection, setTitleDirection] = useState("");
+  const [magDirection, setMagDirection] = useState("");
+  const [timeDirection, setTimeDirection] = useState("");
+  const [col, setCol] = useState("");
+
+  const handleColClick = (e) => {
+    const id = e.target.id;
+    console.log(id);
+    setCol(id);
+    if (id === "title") {
+      setTitleDirection(!titleDirection);
+    } else if (id === "mag") {
+      setMagDirection(!magDirection);
+    } else if (id === "time") {
+      setTimeDirection(!timeDirection);
+    }
+  };
+
+  const sortData = (data) => {
+    data.sort((a, b) => {
+      if (col === "title") {
+        return titleDirection
+          ? a.properties.place > b.properties.place
+          : a.properties.place < b.properties.place;
+      } else if (col === "mag") {
+        return magDirection
+          ? a.properties.place > b.properties.place
+          : a.properties.place < b.properties.place;
+      } else if (col === "time") {
+        return timeDirection
+          ? a.properties.time > b.properties.time
+          : a.properties.time < b.properties.time;
+      }
+    });
+    return data;
+  };
+
   return (
     <>
       <Header
@@ -17,13 +54,31 @@ const HomePage = () => {
         <table className={utilStyles.table}>
           <thead>
             <tr>
-              <th className={utilStyles.tableHeader}>Title</th>
-              <th className={utilStyles.tableHeader}>Magnitude</th>
-              <th className={utilStyles.tableHeader}>Time</th>
+              <th
+                id="title"
+                onClick={handleColClick}
+                className={utilStyles.tableHeader}
+              >
+                Title
+              </th>
+              <th
+                id="mag"
+                onClick={handleColClick}
+                className={utilStyles.tableHeader}
+              >
+                Magnitude
+              </th>
+              <th
+                id="time"
+                onClick={handleColClick}
+                className={utilStyles.tableHeader}
+              >
+                Time
+              </th>
             </tr>
           </thead>
           <tbody>
-            {data.data.features.map((record) => (
+            {sortData(data.data.features).map((record) => (
               <tr key={record.id} className={utilStyles.tableRow}>
                 <td>
                   <a href={`detail/${record.id}`}>{record.properties.place}</a>
